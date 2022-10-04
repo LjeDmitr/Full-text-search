882ada9dd4136d90b0762c935d24aacbfcb8d119
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-10-04 11:03:51 +0700

    Add defining functions in a header file for tests

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -3,3 +3,8 @@
 #include <string>
 
 std::string parseStr(std::string text, std::string stop_words[], int ngram_min_length, int ngram_max_length);
+std::string deletePunct(std::string text);
+std::string stringToLower(std::string text);
+std::vector<std::string> splitWords(std::string text, std::vector<std::string> vector);
+std::vector<std::string> deleteStopWords(std::string stop_words[], std::vector<std::string> vector);
+std::string generateNgrams(std::vector<std::string> vector, int min_ngram_length, int max_ngram_length);
