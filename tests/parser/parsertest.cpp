884837d3c2ee9974dc16b1805285a1e86a382e35
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-10-04 11:22:54 +0700

    Add tests for function deletePunct and function stringToLower

--- tests/parser/parsertest.cpp
+++ tests/parser/parsertest.cpp
@@ -11,3 +11,45 @@ TEST(parser, normalParsingString)
 	string expect = "jek 0 jeky 0 jekyl 0 jekyll 0 hyd 1 hyde 1";
 	ASSERT_EQ(expect, result);
 }
+
+TEST(deletePuncts, correctData)
+{
+	string result = deletePunct("Hello, World.");
+	string expect = "Hello World";
+	ASSERT_EQ(expect, result);
+}
+
+TEST(deletePuncts, noPunctString)
+{
+	string result = deletePunct("Hello World");
+	string expect = "Hello World";
+	ASSERT_EQ(expect, result);
+}
+
+TEST(deletePuncts, checkViodString)
+{
+	string result = deletePunct("");
+	string expect = "";
+	ASSERT_EQ(expect, result);
+}
+
+TEST(stringToLow, correctData)
+{
+	string result = stringToLower("Hello World");
+	string expect = "hello world";
+	ASSERT_EQ(expect, result);
+}
+
+TEST(stringToLow, noLowerCase)
+{
+	string result = stringToLower("hello world");
+	string expect = "hello world";
+	ASSERT_EQ(expect, result);
+}
+
+TEST(stringToLow, checkViodString)
+{
+	string result = stringToLower("");
+	string expect = "";
+	ASSERT_EQ(expect, result);
+}
