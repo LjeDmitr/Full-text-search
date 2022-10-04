dab446dc604525bd1bd26c5726ba7e307b12663a
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-10-04 12:27:42 +0700

    Fix bug changed an array of strings to a vector of strings

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	string text = "Dr. Jekyll and Mr. Hyde";
-	string stop_words[] = {"and", "dr", "mr"}; 
+	vector<string> stop_words = {"and", "dr", "mr"}; 
 	int ngram_min_length = 3;
 	int ngram_max_length = 6;
 	cout << parseStr(text, stop_words, ngram_min_length, ngram_max_length) << endl;
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -37,16 +37,16 @@ vector<string> splitWords(string text, vector<string> vector) {
 	return vector;
 }
 
-vector<string> deleteStopWords(string stop_words[], vector<string> vector) {
-	auto iter = vector.cbegin();
-	for (int i = 0; i < (int)vector.size(); ++i) {
-		for (int j = 0; j < (int)stop_words->size(); ++j) {
-			if (vector[i] == stop_words[j]) {
-				vector.erase(iter + i);
+vector<string> deleteStopWords(vector<string> stop_words, vector<string> words) {
+	auto iter = words.cbegin();
+	for (int i = 0; i < (int)words.size(); ++i) {
+		for (int j = 0; j < (int)stop_words.size(); ++j) {
+			if (words[i] == stop_words[j]) {
+				words.erase(iter + i);
 			}
 		}
 	}
-	return vector;
+	return words;
 }
 
 string generateNgrams(vector<string> vector, int min_ngram_length, int max_ngram_length) {
@@ -60,7 +60,7 @@ string generateNgrams(vector<string> vector, int min_ngram_length, int max_ngram
     return result;
 }
 
-string parseStr(string text, string stop_words[], int ngram_min_length, int ngram_max_length) {
+string parseStr(string text, vector<string> stop_words, int ngram_min_length, int ngram_max_length) {
 	text = deletePunct(text);
 	text = stringToLower(text);
 	vector<string> words;
