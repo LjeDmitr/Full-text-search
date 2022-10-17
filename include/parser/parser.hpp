5f203369e989078c9f88992e01a0d760e05cdd90
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-10-17 22:45:33 +0700

    Make inner functions static

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -3,9 +3,4 @@
 #include <string>
 #include <vector>
 
-std::string parseStr(std::string text, std::vector<std::string> stop_words, int ngram_min_length, int ngram_max_length);
-std::string deletePunct(std::string text);
-std::string stringToLower(std::string text);
-std::vector<std::string> splitWords(std::string text, std::vector<std::string> vector);
-std::vector<std::string> deleteStopWords(std::vector<std::string> stop_words, std::vector<std::string> vector);
-std::string generateNgrams(std::vector<std::string> vector, int min_ngram_length, int max_ngram_length);
+std::string parseStr(std::string text, int ngram_min_length, int ngram_max_length);
