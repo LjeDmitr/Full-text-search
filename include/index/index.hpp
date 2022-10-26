d154c6214e88b5d88acd2f7f83802d4ecc51a833
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-10-27 00:00:07 +0700

    Define a new class 'indexBuilder'

--- include/index/index.hpp
+++ include/index/index.hpp
@@ -11,3 +11,14 @@ class Index
 		std::pair<std::string, std::string> getDocs();
 		std::pair<std::string, std::vector<term>> getEntries();
 };
+
+class indexBuilder
+{
+	private:
+		std::vector<Index> indexes;
+	public:
+		void add_document(std::string document_id, std::string text);
+		void setIndexes(Index index);
+		std::vector<Index> getIndexes();
+		Index index(std::string doc_key, std::string doc_text, std::string entries_key, std::vector<term> entries_terms);
+};
