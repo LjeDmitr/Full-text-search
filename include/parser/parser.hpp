686c9d9cc39ccd0d4aeeb884a65e5349e1fb167b
Dmitry Stacenko <dimastacenko104@gmail.com>
2022-11-08 21:34:41 +0700

    Fix code style

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -3,14 +3,14 @@
 #include <string>
 #include <vector>
 
-class parser
-{
-	private:
-		std::vector<std::pair<std::string, std::vector<int>>> ngrams;
-		std::string parsing_str;
-	public:
-		void parseStr(std::string text, int ngram_min_length, int ngram_max_length);
-		void createNewVector(int first_pos);
-		std::string getParsingStr();
-		std::vector<std::pair<std::string, std::vector<int>>> getNgrams();
+class parser {
+ private:
+  std::vector<std::pair<std::string, std::vector<int>>> ngrams;
+  std::string parsing_str;
+
+ public:
+  void parseStr(std::string text, int ngram_min_length, int ngram_max_length);
+  void createNewVector(int first_pos);
+  std::string getParsingStr();
+  std::vector<std::pair<std::string, std::vector<int>>> getNgrams();
 };
