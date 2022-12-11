9f38af42e64f7932ecfd00d7d71fceca35f03539
Dmitry <dimastacenko104@gmail.com>
2022-12-12 02:01:47 +0700

    First version search in index

--- include/search/search.hpp
+++ include/search/search.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+struct search_info {
+  int df;
+  std::string text;
+  std::vector<std::pair<std::string, int>> tf;
+};
+
+class SearchIndex {
+ private:
+  std::vector<search_info> quers;
+  std::vector<std::pair<std::string, double>> search_result;
+
+ public:
+  void search(std::string str, std::string path_index);
+  void score(std::string doc_id);
+  std::vector<std::pair<std::string, double>> getSearchResult();
+};
---
+++ src/search/search.cpp
@@ -0,0 +1,78 @@
+#include <dirent.h>
+#include <picosha2.h>
+#include <cmath>
+#include <fstream>
+#include <parser/parser.hpp>
+#include <search/search.hpp>
+
+using namespace std;
+
+static search_info docInfo(search_info query, vector<string> entries) {
+  for (size_t i = 2; i < entries.size(); i++) {
+    query.tf.push_back(make_pair(entries[i], stoi(entries[i + 1])));
+    i++;
+    i += stoi(entries[i]);
+  }
+  return query;
+}
+
+void SearchIndex::search(string str, string path_index) {
+  parser parsing_string;
+  parsing_string.parseStr(str);
+  string hash_hex_str, buff;
+  vector<pair<string, vector<int>>> ngrams = parsing_string.getNgrams();
+  ifstream index;
+  for (size_t i = 0; i < ngrams.size(); i++) {
+    picosha2::hash256_hex_string(ngrams[i].first.substr(0, 3), hash_hex_str);
+    hash_hex_str = hash_hex_str.substr(0, 6);
+    index.open(path_index + hash_hex_str);
+    if (index.is_open()) {
+      getline(index, buff);
+      vector<string> entries;
+      entries = parser::splitWords(buff, entries);
+      struct search_info query;
+      if (ngrams[i].first == entries[0]) {
+        query.df = stoi(entries[1]);
+        query.text = entries[0];
+        query = docInfo(query, entries);
+      }
+      quers.push_back(query);
+    }
+  }
+}
+
+static int termFrequency(vector<pair<string, int>> query_tf, string doc_id) {
+  for (size_t i = 0; i < query_tf.size(); i++) {
+    if (query_tf[i].first == doc_id) {
+      return query_tf[i].second;
+    }
+  }
+  return 0;
+}
+
+static int countDoc() {
+  int file_count = 0;
+  DIR* dirp;
+  struct dirent* entry;
+  dirp = opendir("index/doc");
+  while ((entry = readdir(dirp)) != NULL) {
+    if (entry->d_type == DT_REG) {
+      file_count++;
+    }
+  }
+  closedir(dirp);
+  return file_count;
+}
+
+void SearchIndex::score(string doc_id) {
+  int N = countDoc();
+  double score = 0.0;
+  for (size_t i = 0; i < quers.size(); i++) {
+    score += termFrequency(quers[i].tf, doc_id) * log(N / quers[i].df);
+  }
+  search_result.push_back(make_pair(doc_id, score));
+}
+
+vector<pair<string, double>> SearchIndex::getSearchResult() {
+  return search_result;
+}
