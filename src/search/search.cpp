d829ef6695dd64c8fa1f159e45d1f67fd35a9e0d
Dmitry <dimastacenko104@gmail.com>
2022-12-12 22:11:56 +0700

    Implement search version 1.0

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -2,19 +2,11 @@
 #include <picosha2.h>
 #include <cmath>
 #include <fstream>
+#include <index/index.hpp>
 #include <parser/parser.hpp>
 #include <search/search.hpp>
 
 using namespace std;
-
-static search_info docInfo(search_info query, vector<string> entries) {
-  for (size_t i = 2; i < entries.size(); i++) {
-    query.tf.push_back(make_pair(entries[i], stoi(entries[i + 1])));
-    i++;
-    i += stoi(entries[i]);
-  }
-  return query;
-}
 
 void SearchIndex::search(string str, string path_index) {
   parser parsing_string;
@@ -26,15 +18,17 @@ void SearchIndex::search(string str, string path_index) {
   for (size_t i = 0; i < ngrams.size(); i++) {
     picosha2::hash256_hex_string(ngrams[i].first.substr(0, 3), hash_hex_str);
     hash_hex_str = hash_hex_str.substr(0, 6);
-    index.open(path_index + hash_hex_str);
+    index.open(path_index + "entries/" + hash_hex_str);
     if (index.is_open()) {
       while (getline(index, buff)) {
         entries = parser::splitWords(buff, entries);
         struct search_info query;
         if (ngrams[i].first == entries[0]) {
+          IndexAccessor access;
+          access.genDocList(entries[0]);
           query.df = stoi(entries[1]);
           query.text = entries[0];
-          query = docInfo(query, entries);
+          query.tf = access.getTermDocList();
           quers.push_back(query);
         }
         buff.clear();
@@ -70,9 +64,10 @@ static int countDoc() {
 
 void SearchIndex::score(string doc_id) {
   int N = countDoc();
-  double score = 0.0;
+  double score = 0.0, idf = 0.0;
   for (size_t i = 0; i < quers.size(); i++) {
-    score += termFrequency(quers[i].tf, doc_id) * log(N / quers[i].df);
+    idf = (double)N / (double)quers[i].df;
+    score += termFrequency(quers[i].tf, doc_id) * log(idf);
   }
   if (score != 0) {
     search_result.push_back(make_pair(doc_id, score));
@@ -80,5 +75,8 @@ void SearchIndex::score(string doc_id) {
 }
 
 vector<pair<string, double>> SearchIndex::getSearchResult() {
+  sort(search_result.begin(), search_result.end(), [](const auto& x, const auto& y) {
+    return x.second > y.second;
+  });
   return search_result;
 }
