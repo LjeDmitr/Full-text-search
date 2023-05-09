392b85060e40882a1cf2315e135fef5bc9d7290d
Dmitry <dimastacenko104@gmail.com>
2023-05-09 22:19:08 +0700

    Remake search test

--- tests/search/searchtest.cpp
+++ tests/search/searchtest.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 TEST(search, bye_earth) {
   indexBuilder index_builder;
-  index_builder.add_document("100", "Hello World");
-  index_builder.add_document("101", "Bye World");
-  index_builder.add_document("102", "Hello Earth");
-  textIndexWriter::write("index", index_builder);
+  index_builder.add_document(100, "Hello World");
+  index_builder.add_document(101, "Bye World");
+  index_builder.add_document(102, "Hello Earth");
+  BinaryIndexWriter::write("index", index_builder);
   
   SearchIndex index_search;
   index_search.search("bye earth", "index/");
-  index_search.score("100");
-  index_search.score("101");
-  index_search.score("102");
   vector<pair<string, double>> result = index_search.getSearchResult();
   vector<double> expect = {3.295836, 1.098612};
   ASSERT_EQ(expect.size(), result.size());
@@ -26,16 +23,13 @@ TEST(search, bye_earth) {
 
 TEST(search, hello_world) {
   indexBuilder index_builder;
-  index_builder.add_document("100", "Hello World");
-  index_builder.add_document("101", "Bye World");
-  index_builder.add_document("102", "Hello Earth");
-  textIndexWriter::write("index", index_builder);
+  index_builder.add_document(100, "Hello World");
+  index_builder.add_document(101, "Bye World");
+  index_builder.add_document(102, "Hello Earth");
+  BinaryIndexWriter::write("index", index_builder);
 
   SearchIndex index_search;
   index_search.search("hello world", "index/");
-  index_search.score("100");
-  index_search.score("101");
-  index_search.score("102");
   vector<pair<string, double>> result = index_search.getSearchResult();
   vector<double> expect = {2.432790, 1.216395, 1.216395};
   ASSERT_EQ(expect.size(), result.size());
